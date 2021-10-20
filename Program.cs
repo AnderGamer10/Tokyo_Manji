aa0eaf3307c99f2ecca899327789219af467060d
AnderGamer10 <anderedondo10@hotmail.com>
2021-10-20 20:25:52 +0200

    terminado full

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@ namespace DisfrutarProgramando{
     class Program{
 
         static void Main(string[] args){
-
-            //la lista a trabajar
             List<string> ciudades = new List<string>(){"Tokyo", "London", "Rome", "Donlon", "Kyoto", "Paris"};
 
             var res = ciudAnagrama(ciudades);
@@ -15,8 +13,6 @@ namespace DisfrutarProgramando{
             ToString(res);
 
         }
-
-        //método que devolverá la solución en una lista
         public static List<List<string>> ciudAnagrama(List<string> lista){
 
             List<List<string>> LCAnagrama = new List<List<string>>();
@@ -44,36 +40,7 @@ namespace DisfrutarProgramando{
             return LCAnagrama;
         }
 
-        //método que cambia el orden de las sílabas
-
         public static string cambiarSilabas(string ciudad){
-
-            
-            /* VERSIÓN ANTIGÜA (SOLO FUNCIONA CON LOS CASOS ENTREGADOS)
-
-                //aquí he usado contains para comparar si el caracter del string introducido es una vocal (es bastante interesante para usarlo a futuro)
-
-                //https://docs.microsoft.com/es-es/dotnet/api/system.string.contains?view=net-5.0
-            
-
-                //if(!"aeiou".Contains(Char.ToLower(ciudad[0])) && "aeiou".Contains(Char.ToLower(ciudad[1])) && "n".Contains(Char.ToLower(ciudad[2]))){
-
-                    //SAnagrama = ciudad.Substring(3) + ciudad.Substring(0, 3);
-                //}else if(!"aeiou".Contains(Char.ToLower(ciudad[0])) && "aeiou".Contains(Char.ToLower(ciudad[1]))){
-
-                    //SAnagrama = ciudad.Substring(2) + ciudad.Substring(0, 2);
-                //}else{
-
-                    //SAnagrama = ciudad.Substring(3) + ciudad.Substring(0, 3);
-                //}
-
-            */
-
-            // VERSIÓN NUEVA  (FUNCIONA CON ANAGRAMAS ORDENADOS DE CUALQUIER MANERA)
-
-            // cogí esta idea de reordenar un string alfebéticamente
-
-            //https://www.c-sharpcorner.com/blogs/sort-string-in-alphabetical-order-in-c-sharp
 
             char[] SAnagrama = ciudad.ToCharArray();  
 
@@ -94,9 +61,6 @@ namespace DisfrutarProgramando{
 
             return res;
         }
-
-        //método que devuelve la lista convertida en string
-
         public static void ToString(List<List<string>> lista){
 
                 var sRes = "[\n";
